b46d803336a3ad07c3ee3f6e80b0973a4771a364
DennisRaaijmakers <r0894881@student.thomasmore.be>
2022-12-10 15:03:25 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,19 +1,8 @@
 from fastapi import FastAPI, Query, Path
 from mongoengine import Document, StringField, connect
-from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 connect(db="name", host="mongodb", port=27017, username="dennis", password="abc123!")
-# CORS
-origins = ["*"]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 #class
 class Name(Document):
