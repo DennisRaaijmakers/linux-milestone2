83a344431063e50a92f81cdfa55524f76be68c30
DennisRaaijmakers <r0894881@student.thomasmore.be>
2022-12-10 20:02:36 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,20 +1,10 @@
-from fastapi import FastAPI, Query, Path
-from mongoengine import Document, StringField, connect
+from typing import Union
+
+from fastapi import FastAPI
 
 app = FastAPI()
-connect(db="name", host="mongodb", port=27017, username="dennis", password="abc123!")
-
-#class
-class Name(Document):
-    name = StringField()
 
 
-@app.get("/user")
-async def user():
-    name = Name.objects
-    print ("TEST", name)
-    return {"name" : name}
-
-@app.get("/api/")
-async def user1():
-    return {"Welcome to API"}
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
