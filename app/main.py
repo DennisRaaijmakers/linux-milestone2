668317f2b2c7f343053940ab843863d09bb3eb70
DennisRaaijmakers <r0894881@student.thomasmore.be>
2022-12-11 12:19:28 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,16 @@
 from typing import Union
-
+from pydantic import BaseModel, Field
 from fastapi import FastAPI
 
 app = FastAPI()
 
+connect(db="name", host="mongodb", port=27017, username="dennis", password="abc123!")
 
-@app.get("/user")
-def read_root():
-    return {"Hello": "World"}
+class UserBase(MongoBase):
+    name: str
+
+
+@app.get('/user', response_model=users.UserBase)
+async def namedb():
+    name = await db.users.find()
+    return name
